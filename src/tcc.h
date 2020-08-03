2756769b0a6addfe5d1d3ed1429f4af7bc751a77
Allison G <ahumank@Allisons-MacBook-Pro.local>
2020-08-03 16:52:28 -0400

    fixed counter error

--- src/tcc.h
+++ src/tcc.h
@@ -8,6 +8,7 @@ class TCCGenerator : smt::IdentityWalker
     smt::WalkerStepResult visit_term(smt::Term &t) override; //check Term and Term what input and what output? math to function overriding? New ones?
     smt::Term generate_tcc(smt::Term &t); //in parenthesis? what feed to my function?
     smt::Term convert(smt::Term &t);
+    int counter=0;
 
 //    int counter; //counter for no. of +'s in a given input term
     std::vector<smt::Term> cached_children;
