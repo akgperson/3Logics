16d1439ea87b9b54fe837949ee97916e3a8fe6fc
yoni206 <yoni206@gmail.com>
2020-08-24 23:15:55 -0700

    another change in smt switch

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -33,7 +33,8 @@ if [ ! -d "$DEPS/smt-switch" ]; then
     git clone https://github.com/makaimann/smt-switch
     cd smt-switch
 #    git checkout -f 969a5c0850e639c5547f0e6166491c85bd2e1e4d
-    git checkout -f 15d031bd3d673f0b39765534fd1fefd872c45ed4
+#    git checkout -f 15d031bd3d673f0b39765534fd1fefd872c45ed4
+    git checkout -f 245622786fea1fb6b061c55ba36fae3d5842d597
     ./contrib/setup-cvc4.sh
     ./configure.sh --cvc4 --msat --msat-home=../mathsat --prefix=local --static $CONF_OPTS
     cd build
